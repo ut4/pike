44a0aebcca9b75f1c486480344a24b139402607b
ut4 <ut4.l@live.fi>
2020-05-06 14:12:14 +0300

    Korvaa examples/esimerkin-nimi.php -> examples/EsimerkinNimi/index.php.

--- examples/HelloWorld/index.php
+++ examples/HelloWorld/index.php
@@ -1,9 +1,9 @@
 <?php
 
-// Katso myös: /docs/fi/examples/hello-world.md
+// Katso myös: ut4.github.io/pike/examples/hello-world.html
 
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
-$loader->addPsr4('Me\\HelloWorld\\', __DIR__ . '/HelloWorld/src');
+$loader = require dirname(__DIR__, 2) . '/vendor/autoload.php';
+$loader->addPsr4('Me\\HelloWorld\\', __DIR__ . '/src');
 
 $myModules = [\Me\HelloWorld\SomeModule::class];
 $app = \Pike\App::create($myModules);
--- examples/MappingRoutes/index.php
+++ examples/MappingRoutes/index.php
@@ -1,9 +1,9 @@
 <?php
 
-// Katso myös: /docs/fi/examples/mapping-routes.md
+// Katso myös: ut4.github.io/pike/examples/mapping-routes.html
 
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
-$loader->addPsr4('Me\\MappingRoutes\\', __DIR__ . '/MappingRoutes/src');
+$loader = require dirname(__DIR__, 2) . '/vendor/autoload.php';
+$loader->addPsr4('Me\\MappingRoutes\\', __DIR__ . '/src');
 
 $myModules = [\Me\MappingRoutes\Module::class];
 $app = \Pike\App::create($myModules);
